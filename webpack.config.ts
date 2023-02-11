0650c37b898721159bfdcc6e4f58cd165307f7bf
ylesandale <ylesandale7@gmail.com>
2023-02-11 21:56:27 +0400

    webpack start setup & add typescript

---
+++ webpack.config.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config: webpack.Configuration = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "[name].[contenthash].js",
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: path.resolve(__dirname,'public','index.html')
+    }),
+        new webpack.ProgressPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+}
+export default config
