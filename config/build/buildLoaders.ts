5957d8c0eda0a96dad11cdb299c7ed0328f12299
ylesandale <ylesandale7@gmail.com>
2023-02-12 17:28:50 +0400

    add react & scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,19 @@
 import { RuleSetRule } from "webpack";
 
 export function buildLoaders(): RuleSetRule[] {
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
+
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -9,5 +22,6 @@ export function buildLoaders(): RuleSetRule[] {
 
     return [
         typescriptLoader,
+        cssLoader
     ]
 }
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import {render} from "react-dom";
+import {Counter} from "./componentss/Counter";
+
+render(<div>
+    <Counter/>
+</div>, document.getElementById('root'))
