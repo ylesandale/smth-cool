8ef5bd0af7177cec645dee5f648e3f98bb0cbd7e
ylesandale <ylesandale7@gmail.com>
2023-02-12 20:51:15 +0400

    add lazy load for chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Counter } from "./components/Counter";
+import {Link, Route, Routes} from "react-router-dom";
 import "./index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
+import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
+import {Suspense} from "react";
 
 const App = () => {
     return (
         <div className="app">
-            <Counter/>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 }
